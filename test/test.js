d9dfd47c1928d24e2cc09539dcf511d8a971de13
Robert <robert.simion@transsped.ro>
2019-10-15 20:28:07 +0300

    credential/authSignature

--- test/test.js
+++ test/test.js
@@ -3,6 +3,12 @@
 const cscServer = require('../lib');
 
 const server = cscServer.createServer({ database: undefined });
-server.generateCredentials('robert.simion', (err) => {
-    if(err) console.log(err);
+
+// server.generateCredentials('robert.simion', (err) => {
+//     if(err) console.log(err);
+// });
+
+server.listen(null, null, (error) => {
+    if (error) console.log('Test Error!' + error);
+    console.log('Listening...')
 });
